48ba242a58562a7871e62237d1d58c0c6f035743
Muhammad Bin Sohail <templergreek@gmail.com>
2024-10-20 11:16:27 +0500

    Initial Commit

---
+++ NegotiateX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NegotiateX
+//
+//  Created by Muhammad Bin Sohail on 10/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NegotiateX/NegotiateXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegotiateXApp.swift
+//  NegotiateX
+//
+//  Created by Muhammad Bin Sohail on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct NegotiateXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
